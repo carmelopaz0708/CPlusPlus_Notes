2bdd891fa27c12e5ff7b6cb5d26c6b74efd0be55
Siege Paz <carmelopaz0708@gmail.com>
2019-02-06 19:01:54 +0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/*
+  hello.cpp
+  Every introduction starts with a hello.
+*/
+
+#include <iostream>                   // Header. Includes input and output capability for the code
+
+using namespace std;                  // 'using' directive wherein std is a namespace. Tells compiler which class/namespace to look for
+                                      // as an identifier
+
+int main() {                          // Program's main entry point. All C++ programs start in main()
+  cout << "Hello, world!" << endl;    // cout stands for compiler output. Output this string to the compiler. endl stands for end line
+}
