10384e260eeeb09952364a06cad34eb74728d1dc
Siege Paz <carmelopaz0708@gmail.com>
2019-02-08 19:56:07 +0800

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,60 @@
+/*
+  function.cpp
+  Functions segments blocks of code in your program. It allows the user to isolate specific tasks to specific areas around the program.   That way, the program is easier to debug. Plus, it looks nice.
+*/
+
+#include<iostream>
+
+// Function prototypes. Required when creating new functions. Syntax is 'funcType funcName(dataType ...)'
+double sum(double, double);
+double dif(double, double);
+double mul(double, double);
+double quo(double, double);
+void disp(double, double, double, double, double, double);
+
+int main() {
+    using namespace std;
+    
+    double val1, val2, sum_val, dif_val, mul_val, quo_val;
+    cout << "Enter first variable: ";
+    cin >> val1;
+    cout << "Enter second variable: ";
+    cin >> val2;
+    cout << endl;
+    
+    sum_val = sum(val1, val2);
+    dif_val = dif(val1, val2);
+    mul_val = mul(val1, val2);
+    quo_val = quo(val1, val2);
+    
+    disp(val1, val2, sum_val, dif_val, mul_val, quo_val);
+}
+
+double sum(double add1, double add2) {
+    double x = add1 + add2;
+    return x;
+}
+
+double dif(double dif1, double dif2) {
+    double x = dif1 - dif2;
+    return x;
+}
+
+double mul(double mul1, double mul2) {
+    double x = mul1 * mul2;
+    return x;
+}
+
+double quo(double quo1, double quo2) {
+    double x = quo1 / quo2;
+    return x;
+}
+
+void disp(double val1, double val2, double sum_val, double dif_val, double mul_val, double quo_val) {
+    using namespace std;
+    cout << val1 << " + " << val2 << " = " << sum_val << endl;
+    cout << val1 << " - " << val2 << " = " << dif_val << endl;
+    cout << val1 << " * " << val2 << " = " << mul_val << endl;
+    cout << val1 << " / " << val2 << " = " << quo_val << endl;
+    cout << endl;
+}
