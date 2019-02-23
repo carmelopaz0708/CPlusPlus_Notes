718a30c842d6d6ff876fd85c578added6acac4f1
Siege Paz <carmelopaz0708@gmail.com>
2019-02-23 18:42:16 +0800

    Removed using directive in favor of std::
    The using directive is bad practice in C++ Programming.

--- hello.cpp
+++ hello.cpp
@@ -5,9 +5,6 @@
 
 #include <iostream>                   // Header. Includes input and output capability for the code
 
-using namespace std;                  // 'using' directive wherein std is a namespace. Tells compiler which class/namespace to look for
-                                      // as an identifier
-
-int main() {                          // Program's main entry point. All C++ programs start in main()
-  cout << "Hello, world!" << endl;    // cout stands for compiler output. Output this string to the compiler. endl stands for end line
+int main() {                                    // Program's main entry point. All C++ programs start in main()
+  std::cout << "Hello, world!" << std::endl;    // cout stands for compiler output. Output this string to the compiler. endl stands for end line
 }
