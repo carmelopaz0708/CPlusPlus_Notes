94c21899b69a0abbb560200b224e38d0175f782b
Siege Paz <carmelopaz0708@gmail.com>
2019-03-06 21:27:57 +0800

    Reupdated program with verbose explanation

--- function.cpp
+++ function.cpp
@@ -1,57 +1,68 @@
 /*
-  function.cpp
-  Functions segments blocks of code in your program. It allows the user to isolate specific tasks to specific areas around the program.   That way, the program is easier to debug. Plus, it looks nice.
+	FUNCTION AND FUNCTION CALL
+		Functions are a very useful method of grouping complicated lines of code together. Suppose a program has several thousand lines of code, reading the program from top to bottom would be difficult. Luckily for us, we can use a function to group together lines of codes that perform a specific function. This is to isolate code from one area to another and prevent generating bugs which are difficult to trace. By partitioning code into functions, our program becomes easily readable by fellow programmers reading and debugging your code. Functions also isolate variables from other parts of the code, allowing you to reuse variable names. A variable initialized inside a function will only exist inside that function.
+		C++, Java and C programming make use of a special function called main(). 'main()' pertains to your home function; it is where the function starts. Other programming languages such as Python does not make use of main(). When the program is run, the main function is called first. For other functions, we require a function prototype.
+		A function prototype is a header file placed on top of the program that declares the name as a function. They follow the syntax 'type name(args)' where type refers to its data type (int, double, void, etc.), name is the name of the function and args as the input arguments to the function. You must declare all input arguments in the functions prototype to avoid generating an error. Once the function prototype has been declared, you can generate your function inside of the program.
+		Generating a function follows the same syntax as a function header but only this time you can declare names for your input arguments. To obtain a value from a function, use a return statement. The return statement takes the output of a particular function, enabling it to be used by other functions.
+		Function call means to allow the function to execute its lines of code. This can be done by simply calling the name of the function (and assigning its input arguments if (neccesary) or assigning a variable to a function call (as shown below).
+
+	@auth siege
+	@init 03/06/19
 */
 
-#include<iostream>
+#include <iostream>
 
-// Function prototypes. Required when creating new functions. Syntax is 'funcType funcName(dataType ...)'
-double sum(double, double);
-double dif(double, double);
-double mul(double, double);
-double quo(double, double);
-void disp(double, double, double, double, double, double);
+double summer(double, double);
+double difference(double, double);
+double multiplier(double, double);
+double divider(double, double);
+void display(double, double, double, double);
 
 int main() {
-    double val1, val2, sum_val, dif_val, mul_val, quo_val;
-    std::cout << "Enter first variable: ";
-    std::cin >> val1;
-    std::cout << "Enter second variable: ";
-    std::cin >> val2;
-    std::cout << std::endl;
-    
-    sum_val = sum(val1, val2);
-    dif_val = dif(val1, val2);
-    mul_val = mul(val1, val2);
-    quo_val = quo(val1, val2);
-    
-    disp(val1, val2, sum_val, dif_val, mul_val, quo_val);
+	double val1, val2, sum, dif, prod, quo;
+	std::cout << "Enter val1: ";
+	std::cin >> val1;
+	std::cout << "Enter val2: ";
+	std::cin >> val2;
+
+	sum = summer(val1, val2);
+	dif = difference(val1, val2);
+	prod = multiplier(val1, val2);
+	quo = divider(val1, val2);
+
+	display(sum, dif, prod, quo);
 }
 
-double sum(double add1, double add2) {
-    double x = add1 + add2;
-    return x;
+double summer(double x, double y) {
+	double z;
+	z = x + y;
+	return z;
 }
 
-double dif(double dif1, double dif2) {
-    double x = dif1 - dif2;
-    return x;
+double difference(double x, double y) {
+	double z;
+	z = x - y;
+	return z;
 }
 
-double mul(double mul1, double mul2) {
-    double x = mul1 * mul2;
-    return x;
+double multiplier(double x, double y) {
+	double z;
+	z = x * y;
+	return z;
 }
 
-double quo(double quo1, double quo2) {
-    double x = quo1 / quo2;
-    return x;
+double divider(double x, double y) {
+	double z;
+	z = x / y;
+	return z;
 }
 
-void disp(double val1, double val2, double sum_val, double dif_val, double mul_val, double quo_val) {
-    std::cout << val1 << " + " << val2 << " = " << sum_val << std::endl;
-    std::cout << val1 << " - " << val2 << " = " << dif_val << std::endl;
-    std::cout << val1 << " * " << val2 << " = " << mul_val << std::endl;
-    std::cout << val1 << " / " << val2 << " = " << quo_val << std::endl;
-    std::cout << std::endl;
+void display(double ans1, double ans2, double ans3, double ans4) {
+	std::cout << "----------------------------------------------------------\n";
+	std::cout << "val1 + val2 = " << ans1 << "\n";
+	std::cout << "val1 - val2 = " << ans2 << "\n";
+	std::cout << "val1 * val2 = " << ans3 << "\n";
+	std::cout << "val1 / val2 = " << ans4 << "\n";
+	std::cout << "                 END TRANSMISSION" << "\n";
+	std::cout << "----------------------------------------------------------\n";
 }
