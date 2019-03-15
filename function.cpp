86f043abe7759bf5b5267ce9d34b0f49c0f08fcf
Siege Paz <carmelopaz0708@gmail.com>
2019-03-16 02:33:40 +0800

    Adjusted tab spaces

--- function.cpp
+++ function.cpp
@@ -28,50 +28,50 @@ double divider(double, double);
 void display(double, double, double, double);
 
 int main() {
-	double val1, val2, sum, dif, prod, quo;
-	std::cout << "Enter val1: ";
-	std::cin >> val1;
-	std::cout << "Enter val2: ";
-	std::cin >> val2;
+double val1, val2, sum, dif, prod, quo;
+std::cout << "Enter val1: ";
+std::cin >> val1;
+std::cout << "Enter val2: ";
+std::cin >> val2;
 
-	sum = summer(val1, val2);
-	dif = difference(val1, val2);
-	prod = multiplier(val1, val2);
-	quo = divider(val1, val2);
+sum = summer(val1, val2);
+dif = difference(val1, val2);
+prod = multiplier(val1, val2);
+quo = divider(val1, val2);
 
-	display(sum, dif, prod, quo);
+display(sum, dif, prod, quo);
 }
 
 double summer(double x, double y) {
-	double z;
-	z = x + y;
-	return z;
+double z;
+z = x + y;
+return z;
 }
 
 double difference(double x, double y) {
-	double z;
-	z = x - y;
-	return z;
+double z;
+z = x - y;
+return z;
 }
 
 double multiplier(double x, double y) {
-	double z;
-	z = x * y;
-	return z;
+double z;
+z = x * y;
+return z;
 }
 
 double divider(double x, double y) {
-	double z;
-	z = x / y;
-	return z;
+double z;
+z = x / y;
+return z;
 }
 
 void display(double ans1, double ans2, double ans3, double ans4) {
-	std::cout << "----------------------------------------------------------\n";
-	std::cout << "val1 + val2 = " << ans1 << "\n";
-	std::cout << "val1 - val2 = " << ans2 << "\n";
-	std::cout << "val1 * val2 = " << ans3 << "\n";
-	std::cout << "val1 / val2 = " << ans4 << "\n";
-	std::cout << "                 END TRANSMISSION" << "\n";
-	std::cout << "----------------------------------------------------------\n";
+std::cout << "----------------------------------------------------------\n";
+std::cout << "val1 + val2 = " << ans1 << "\n";
+std::cout << "val1 - val2 = " << ans2 << "\n";
+std::cout << "val1 * val2 = " << ans3 << "\n";
+std::cout << "val1 / val2 = " << ans4 << "\n";
+std::cout << "                 END TRANSMISSION" << "\n";
+std::cout << "----------------------------------------------------------\n";
 }
