90d9a97456b90b4b244beb1a336b52ea3579bcea
Siege Paz <carmelopaz0708@gmail.com>
2019-03-20 04:44:54 +0800

    Update overflow.cpp

--- overflow.cpp
+++ overflow.cpp
@@ -1,13 +1,13 @@
 /*
-  INTEGER OVERFLOW
-  An integer overflow occurs when an arithmetic operation tries to create a numeric value exceeding the range of the initialized variable - either higher than the maximum 
-  or lower than the minimum representable value. Overflow causes unintended program behavior and in some cases, jeopardizes the integrity of the program. Such an event can be
-  avoided by initializing the right primitive values to variables used in the program. The coder should also ensure that arithmetic values stay within the boundaries of their
-  maximum and minimum values and that no operation inside the program should exceed the set range.
-  The following program displays the effects of integer overflow by exceeding the maximum or minimum value by one.
+ INTEGER OVERFLOW
+ An integer overflow occurs when an arithmetic operation tries to create a numeric value exceeding the range of the initialized variable - either higher than the maximum 
+or lower than the minimum representable value. Overflow causes unintended program behavior and in some cases, jeopardizes the integrity of the program. Such an event can be
+avoided by initializing the right primitive values to variables used in the program. The coder should also ensure that arithmetic values stay within the boundaries of their
+maximum and minimum values and that no operation inside the program should exceed the set range.
+ The following program displays the effects of integer overflow by exceeding the maximum or minimum value by one.
   
-  @init siege
-  @rev 03/20/19
+@init siege
+@rev 03/20/19
 */
 
 void short_overflow_function(short);
@@ -19,10 +19,10 @@ int main() {
   short input_overflow_value;
   std::cout << "Enter a value: ";
   std::cin >> input_overflow_value;
-  short_overflow_func(input_overflow_value);
+  short_overflow_function(input_overflow_value);
 }
 
-void short_overflow_func(short short_overflow_value) {
+void short_overflow_function(short short_overflow_value) {
   short exceed_max_value, exceed_min_value;
   std::cout << "---------------------------------------------\n";
   std::cout << "Current minimum short value: " << SHRT_MIN << "\n";
@@ -30,6 +30,6 @@ void short_overflow_func(short short_overflow_value) {
   std::cout << "---------------------------------------------\n";
   exceed_min_value = SHRT_MIN - short_overflow_value;
   exceed_max_value = SHRT_MAX + short_overflow_value;
-  std::cout << "SHRT_MIN - " << short_overflow_value << " = " << exceed_min_value << "\n";
-  std::cout << "SHRT_MAX + " << short_overflow_value << " = " << exceed_max_value << "\n";
+  std::cout << "Exceed minimum: "<< SHRT_MIN << " - " << short_overflow_value << " = " << exceed_min_value << "\n";
+  std::cout << "Exceed maximum: "<< SHRT_MAX << " + " << short_overflow_value << " = " << exceed_max_value << "\n";
 }
